8997332f8f70208e2dc2fba9f3dda3326cae892e
Sung Je Kim <rlatjdwp0211@naver.com>
2023-05-14 23:54:40 +0900

    backjoon-11052-카드구매하기

---
+++ backjoon/11052-카드구매하기/index.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.stdin = open("backjoon/11052-카드구매하기/input.txt")
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+price = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(N + 1)]
+
+
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + price[j])
+
+print(dp[i])
