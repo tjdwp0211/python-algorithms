b3baf46ba4334a2e27d95168c1a92edca948e981
Sung Je Kim <rlatjdwp0211@naver.com>
2023-05-24 16:44:04 +0900

    backjoon

--- backjoon/완-1152-단어의개수/index.py
+++ backjoon/완-1152-단어의개수/index.py
@@ -1,5 +1,5 @@
 import sys
 
-sys.stdin = open("backjoon/1152-단어의개수/input.txt")
+sys.stdin = open("backjoon/완-1152-단어의개수/input.txt")
 
 print(len(input().split()))
--- backjoon/완-1157-단어공부/index.py
+++ backjoon/완-1157-단어공부/index.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.stdin = open("backjoon/1157-단어공부/input.txt")
+sys.stdin = open("backjoon/완-1157-단어공부/input.txt")
 
 from collections import Counter
 
