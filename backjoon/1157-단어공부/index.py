6286cb26044776b125b60c9c629ff9ba98c42f64
Sung Je Kim <rlatjdwp0211@naver.com>
2023-05-24 15:56:15 +0900

    backjoon-1157-단어공부

---
+++ backjoon/1157-단어공부/index.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.stdin = open("backjoon/1157-단어공부/input.txt")
+
+from collections import Counter
+
+sorted_string = sorted(Counter(input().upper()).items(), key=lambda x: -x[1])
+max_s, max_cnt = sorted_string[0]
+
+if max_cnt in [cnt for s, cnt in sorted_string[1 : len(sorted_string)]]:
+    print("?")
+else:
+    print(f"{max_s}")
