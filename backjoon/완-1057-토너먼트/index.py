cea2a479d06061408c7162fda19757c4b0b1b2c3
Sung Je Kim <rlatjdwp0211@naver.com>
2023-06-03 15:55:22 +0900

    backjoon-1057-토너먼트

---
+++ backjoon/완-1057-토너먼트/index.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.stdin = open("backjoon/완-1057-토너먼트/input.txt")
+
+from math import *
+
+T = int(input())
+
+
+for test_case in range(1, T + 1):
+    total_num, A_player, B_player = map(int, input().split())
+    round_count = 0
+
+    while ceil(A_player) != ceil(B_player):
+        A_player = ceil(A_player / 2)
+        B_player = ceil(B_player / 2)
+        print("A:", A_player, "| B:", B_player)
+        round_count += 1
+    print(round_count)
